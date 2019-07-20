98b61eaf85ad224dd46c836790d8c555266212b9
Ian Hovde <ihovde@maine.rr.com>
2019-07-20 14:16:14 -0400

    Testing with CSV file

--- sbrLines.py
+++ sbrLines.py
@@ -1,18 +1,24 @@
 from selenium import webdriver
+import csv
+
+with open("testResultsSelenium.csv", "w") as f:
+    f.write("team, Line \n")
 
 driver = webdriver.Firefox()
 
 driver.get("https://classic.sportsbookreview.com/betting-odds/mlb-baseball/")
 
-#teams = driver.find_elements_by_xpath('//span[@class="team-name"]')
+teams = driver.find_elements_by_xpath('//span[@class="team-name"]')
 #lines = driver.find_elements_by_xpath('//div[@class="eventLine-book-value"]')
-team = driver.find_elements_by_xpath('//span[@rel="635"]')
+#team = driver.find_elements_by_xpath('//span[@rel="635"]')
 book = driver.find_elements_by_xpath('//div[@rel="169"]')
 
 
-num_items = len(team)
-for i in range(1):
-    print(team[i].text + " : " + book[i+1].text[0:3])
-#    print(book[i].text + " : " + teams[i-1].text)
+num_items = len(teams)
+with open("testResultsSelenium.csv", "a") as f:
+    for i in range(5):
+        f.write(teams[i].text + "," + book[i].text + "\n")
+#        print(team[i].text + " : " + book[i+1].text[0:3])
+#       print(book[i].text + " : " + teams[i-1].text)
 
 driver.close()
