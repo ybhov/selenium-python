66b1d3bb3cdfdf188c583dee201e34ec456b1d51
Ian Hovde <ihovde@maine.rr.com>
2019-07-21 11:45:00 -0400

    modified loop

--- sbrLines.py
+++ sbrLines.py
@@ -13,11 +13,20 @@ teams = driver.find_elements_by_xpath('//span[@class="team-name"]')
 #team = driver.find_elements_by_xpath('//span[@rel="635"]')
 book = driver.find_elements_by_xpath('//div[@rel="169"]')
 
-
+second = "placeholder"
 num_items = len(teams)
 with open("testResultsSelenium.csv", "a") as f:
     for i in range(5):
-        f.write(teams[i].text + "," + book[i].text + "\n")
+        lines = book[i+1].text
+        line1 = lines.split("\n")[0]
+        line2 = lines.split("\n")[1]
+        if i == 0:
+            f.write(teams[i].text + "," + line1 + "\n")
+        elif i % 2 == 1:
+            f.write(teams[i].text + "," + line1 + "\n")
+        else:
+            f.write(teams[i].text + "," + second + "\n")
+        second = line2
 #        print(team[i].text + " : " + book[i+1].text[0:3])
 #       print(book[i].text + " : " + teams[i-1].text)
 
