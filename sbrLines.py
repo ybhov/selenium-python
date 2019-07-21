28a1c0fea232112f78bfca7a27ac6f88ce49505f
Ian Hovde <ihovde@maine.rr.com>
2019-07-21 12:39:05 -0400

    Added bovada, pinnacle, and heritage

--- sbrLines.py
+++ sbrLines.py
@@ -2,32 +2,41 @@ from selenium import webdriver
 import csv
 
 with open("testResultsSelenium.csv", "w") as f:
-    f.write("team, Line \n")
+    f.write("Team, Heritage, Bovada, Pinnacle \n")
 
 driver = webdriver.Firefox()
 
 driver.get("https://classic.sportsbookreview.com/betting-odds/mlb-baseball/")
 
 teams = driver.find_elements_by_xpath('//span[@class="team-name"]')
-#lines = driver.find_elements_by_xpath('//div[@class="eventLine-book-value"]')
-#team = driver.find_elements_by_xpath('//span[@rel="635"]')
-book = driver.find_elements_by_xpath('//div[@rel="169"]')
+heritage = driver.find_elements_by_xpath('//div[@rel="169"]')
+bovada = driver.find_elements_by_xpath('//div[@rel="999996"]')
+pinnacle = driver.find_elements_by_xpath('//div[@rel="238"]')
 
 second = "placeholder"
 num_items = len(teams)
+j = 1
 with open("testResultsSelenium.csv", "a") as f:
-    for i in range(5):
-        lines = book[i+1].text
-        line1 = lines.split("\n")[0]
-        line2 = lines.split("\n")[1]
+    for i in range(30):
+        hLines = heritage[j].text
+        hLine1 = hLines.split("\n")[0]
+        hLine2 = hLines.split("\n")[1]
+        bLines = bovada[j].text
+        bLine1 = bLines.split("\n")[0]
+        bLine2 = bLines.split("\n")[1]
+        pLines = pinnacle[j].text
+        pLine1 = pLines.split("\n")[0]
+        pLine2 = pLines.split("\n")[1]
+        teamAndPitcher = teams[i].text
+        team = teamAndPitcher.split(" ")[0]
+        print(team)
         if i == 0:
-            f.write(teams[i].text + "," + line1 + "\n")
+            f.write(team + "," + hLine1 + "," + bLine1 + "," + pLine1 + "\n")
         elif i % 2 == 1:
-            f.write(teams[i].text + "," + line1 + "\n")
+            f.write(team + "," + hLine2 + "," + bLine2 + "," + pLine2 + "\n")
         else:
-            f.write(teams[i].text + "," + second + "\n")
-        second = line2
-#        print(team[i].text + " : " + book[i+1].text[0:3])
-#       print(book[i].text + " : " + teams[i-1].text)
+            f.write(team + "," + hLine1 + "," + bLine1 + "," + pLine1 + "\n")
+        if i % 2 ==1:
+            j = j + 1
 
 driver.close()
